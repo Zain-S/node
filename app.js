d8c8e1030cde590aa162b7db598716456853bfc9
Zain Saeed's MacOS <saeedzain20@gmail.com>
2025-09-29 18:20:19 +0500

    Learn module.exports

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-const names = require('./4.names');
-
-console.log(names);
+const { zain, ali } = require('./4-names');
+const names = require('./4-names');
+const sayHi = require('./5-utils');
+const data = require('./6-alternative-flavors');
+console.log(data);
+sayHi(zain);
+sayHi(ali);
+sayHi(names.zain);
+sayHi(names.ali);
