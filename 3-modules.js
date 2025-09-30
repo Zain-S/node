fd70d2b9bd97005eb25c34f183c1f704ccf19315
Zain Saeed's MacOS <saeedzain20@gmail.com>
2025-09-30 11:49:48 +0500

    Learned Modules

---
+++ 3-modules.js
@@ -0,0 +1,10 @@
+const { zain, ali } = require('./4-names');
+const names = require('./4-names');
+const sayHi = require('./5-utils');
+const data = require('./6-alternative-flavors');
+require('./7-mind-grenade');
+console.log(data);
+sayHi(zain);
+sayHi(ali);
+sayHi(names.zain);
+sayHi(names.ali);
--- app.js
+++
@@ -1,9 +0,0 @@
-const { zain, ali } = require('./4-names');
-const names = require('./4-names');
-const sayHi = require('./5-utils');
-const data = require('./6-alternative-flavors');
-console.log(data);
-sayHi(zain);
-sayHi(ali);
-sayHi(names.zain);
-sayHi(names.ali);
