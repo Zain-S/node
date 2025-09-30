2df81b8e945d8fa638c0b7b27ae8fe8cef2e372d
Zain Saeed's MacOS <saeedzain20@gmail.com>
2025-09-30 17:20:05 +0500

    async

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,5 +1,7 @@
 const { readFileSync, writeFileSync } = require('fs');
 
+console.log('start');
+
 const first = readFileSync('./content/first.txt', 'utf8');
 const second = readFileSync('./content/second.txt', 'utf8');
 
@@ -10,3 +12,6 @@ writeFileSync(
         flag: 'a'
     }
 )
+
+console.log('done with this task');
+console.log('starting the next one');
